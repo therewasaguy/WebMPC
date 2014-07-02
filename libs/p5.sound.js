76c0c214e7975f7a6ffb3dad4b49c41322e62e25
Jason Sigal <jason.sigal@gmail.com>
2014-07-02 01:35:38 -0400

    adding noise to p5sound

--- libs/p5.sound.js
+++ libs/p5.sound.js
@@ -1616,9 +1616,12 @@ var p5SOUND = (function(){
 
   /**
    *  LFO is an oscillator designed to oscillate at a lower frequency
-   *  than humans can hear.
-   *  @param {[type]} freq [description]
-   *  @param {[type]} type [description]
+   *  than humans can hear, and to modulate a parameter. By default, it
+   *  does not connect to the master output, and oscillates at 1Hz. Use
+   *  freqMod() to modulate the frequency of another oscillator, ampMod()
+   *  to modulate the amplitude, and mod() to modulate any other parameter.
+   *  @param {[Number]} freq Frequency of the oscillator (1Hz by default)
+   *  @param {[String]} type Type of oscillator (defaults to 'sine')
    */
   p5.prototype.LFO = function(freq, type) {
     this.started = false;
@@ -1631,6 +1634,7 @@ var p5SOUND = (function(){
     // components
     this.oscillator = this.p5s.audiocontext.createOscillator();
     this.oscillator.frequency.value = freq || 1;
+    this.f = this.oscillator.frequency.value;
     this.oscillator.type = type || 'sine';
 
     // set default output gain
@@ -1659,6 +1663,7 @@ var p5SOUND = (function(){
   };
 
   p5.prototype.LFO.prototype.freq = function(val, t){
+    this.f = val;
     var rampTime = t || 0;
     this.oscillator.frequency.cancelScheduledValues(this.p5s.audiocontext.currentTime);
     this.oscillator.frequency.exponentialRampToValueAtTime(val, rampTime + this.p5s.audiocontext.currentTime);
@@ -1770,6 +1775,172 @@ var p5SOUND = (function(){
     this.osc2.oscillator.frequency.exponentialRampToValueAtTime(val, rampTime + this.p5s.audiocontext.currentTime);
   };
 
+
+// =============================================================================
+//                              Noise Class
+//             inspired by noise.js (c) 2013 Zach Denton, MIT License
+//                http://noisehack.com/generate-noise-web-audio-api/
+// =============================================================================
+
+  /**
+   *  [Noise description]
+   *  @param {[type]} type [description]
+   */
+  p5.prototype.Noise = function(type){
+    this.started = false;
+    this.p5s = p5sound;
+
+    this.buffer = _brownNoise;
+    this.output = this.p5s.audiocontext.createGain();
+
+    // set default output gain
+    this.output.gain.value = 0.5;
+
+    // sterep panning
+    this.panPosition = 0.0;
+    this.panner = audiocontext.createPanner();
+    this.panner.panningModel = 'equalpower';
+    this.panner.distanceModel = 'linear';
+    this.panner.setPosition(0,0,0);
+
+    this.output.connect(this.panner);
+    // this.panner.connect(this.p5s.input);  // maybe not connected to output default
+  };
+
+  // generate noise buffers
+  var _whiteNoise = (function() {
+    var bufferSize = 2 * p5sound.audiocontext.sampleRate;
+    var whiteBuffer = p5sound.audiocontext.createBuffer(1, bufferSize, p5sound.audiocontext.sampleRate);
+    var noiseData = whiteBuffer.getChannelData(0);
+    for (var i = 0; i < bufferSize; i++) {
+      noiseData[i] = Math.random() * 2 - 1;
+    }
+    return whiteBuffer;
+  }());
+
+  var _pinkNoise = (function() {
+    var bufferSize = 2 * p5sound.audiocontext.sampleRate;
+    var pinkBuffer = p5sound.audiocontext.createBuffer(1, bufferSize, p5sound.audiocontext.sampleRate);
+    var noiseData = pinkBuffer.getChannelData(0);
+    var b0, b1, b2, b3, b4, b5, b6;
+    b0 = b1 = b2 = b3 = b4 = b5 = b6 = 0.0;
+    for (var i = 0; i < bufferSize; i++) {
+      var white = Math.random() * 2 - 1;
+      b0 = 0.99886 * b0 + white * 0.0555179;
+      b1 = 0.99332 * b1 + white * 0.0750759;
+      b2 = 0.96900 * b2 + white * 0.1538520;
+      b3 = 0.86650 * b3 + white * 0.3104856;
+      b4 = 0.55000 * b4 + white * 0.5329522;
+      b5 = -0.7616 * b5 - white * 0.0168980;
+      noiseData[i] = b0 + b1 + b2 + b3 + b4 + b5 + b6 + white * 0.5362;
+      noiseData[i] *= 0.11; // (roughly) compensate for gain
+      b6 = white * 0.115926;
+    }
+    return pinkBuffer;
+    }());
+
+  var _brownNoise = (function() {
+    var bufferSize = 2 * p5sound.audiocontext.sampleRate;
+    var brownBuffer = p5sound.audiocontext.createBuffer(1, bufferSize, p5sound.audiocontext.sampleRate);
+    var noiseData = brownBuffer.getChannelData(0);
+    var lastOut = 0.0;
+    for (var i = 0; i< bufferSize; i++){
+      var white = Math.random() * 2 - 1;
+      noiseData[i] = (lastOut + (0.02*white)) / 1.02;
+      lastOut = noiseData[i];
+      noiseData[i] *= 3.5;
+    }
+    return brownBuffer
+    }());
+
+  p5.prototype.Noise.prototype.connect = function(unit){
+    if (!unit) {
+      this.panner.connect(this.p5s.input);
+    } 
+    else if (unit.hasOwnProperty('input')){
+        this.panner.connect(unit.input);
+    }
+    else {
+      this.panner.connect(unit);
+    }
+  };
+
+
+  p5.prototype.Noise.prototype.disconnect = function(unit){
+    this.output.disconnect();
+    this.panner.disconnect();
+    this.output.connect(this.panner);
+  };
+
+  p5.prototype.Noise.prototype.freqMod = function(unit){
+    // unit.oscillator.frequency.cancelScheduledValues(this.p5s.audiocontext.currentTime);
+    this.output.connect(unit.oscillator.detune);
+  };
+
+  p5.prototype.Noise.prototype.ampMod = function(unit){
+    // unit.output.cancelScheduledValues(this.p5s.audiocontext.currentTime);
+    this.output.connect(unit.output);
+  };
+
+
+  p5.prototype.Noise.prototype.start = function() {
+    this.noise = this.p5s.audiocontext.createBufferSource();
+    this.noise.buffer = this.buffer;
+    this.noise.loop = true;
+    this.noise.connect(this.output);
+    console.log(this.output);
+    this.noise.start();
+    this.started = true;
+  };
+
+  /**
+   *  Set type of noise to 'white', 'pink' or 'brown'.
+   *  White is the default.
+   *
+   *  @for  Noise
+   *  @param {[String]} type 'white', 'pink' or 'brown'
+   */
+  p5.prototype.Noise.prototype.setType = function(type) {
+    switch(type){
+      case 'white':
+        this.buffer = _whiteNoise;
+        break;
+      case 'pink':
+        this.buffer = _pinkNoise;
+        break;
+      case 'brown':
+        this.buffer = _brownNoise;
+        break;
+      default:
+        this.buffer = _whiteNoise;
+    }
+    if (this.started){
+      this.stop();
+      this.start();
+    }
+  };
+
+  p5.prototype.Noise.prototype.stop = function() {
+    this.noise.stop();
+    this.started = false;
+  };
+
+  p5.prototype.Noise.prototype.amp = function(a) {
+    this.output.gain.value = a;
+  };
+
+  p5.prototype.Noise.prototype.pan = function(pval) {
+    this.panPosition = pval;
+    pval = pval * 90.0;
+    var xDeg = parseInt(pval);
+    var zDeg = xDeg + 90;
+    if (zDeg > 90) {
+      zDeg = 180 - zDeg;
+    }
+    var x = Math.sin(xDeg * (Math.PI / 180));
+    var z = Math.sin(zDeg * (Math.PI / 180));
+    this.panner.setPosition(x, 0, z);
+  };
 
 // =============================================================================
 //                              AudioIn Class
